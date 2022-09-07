3bdbe17da4e74f7cc8841523975baed0fa64c0fe
Nico Mosquera <nmosquera@infosis.com.ar>
2022-09-07 16:03:37 -0300

    Entrega Clase 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
-import ItemListContainer from './container/ItemListContainer'; 
-
+import ItemListContainer from './container/ItemListContainer';
+import ItemDetailConteiner from './container/ItemDetailContainer'; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent/>
       <ItemListContainer greeting="50% OFF en Futbol" titulo="Articulos"/>
+      <>
+            <BrowserRouter>
+                <NavbarComponent />
+                <Routes>
+                    <Route path="/" element={<ItemListContainer/>} />
+                    <Route path="/pokemon/:id" element={<ItemDetailConteiner />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -23,6 +34,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
